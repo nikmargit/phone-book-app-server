492ff8d37bdc4a5bd8dd118c1fc2e58206a36866
nikmargit <nikola.margit@gmail.com>
2018-06-20 18:49:57 +0200

    add add new entry route

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
 // setup database
 var sqlite3 = require("sqlite3").verbose();
-var bodyParser = require("body-parser").json();
+var bodyParser = require("body-parser");
 var db = new sqlite3.Database(":memory:"); // database will be stored in RAM only
-db.serialize(function() {
+db.serialize(function () {
     db.run(
         "CREATE TABLE IF NOT EXISTS phone_book (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, number TEXT)"
     );
@@ -29,12 +29,15 @@ db.serialize(function() {
 // define api object
 const express = require("express");
 const api = express();
+var jsonParser = bodyParser.json()
+//var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 // define API endpoints
 
 // list all entries
 api.get("/", (req, res) => {
-    db.all("SELECT * FROM phone_book", function(err, row) {
+    db.all("SELECT * FROM phone_book", function (err, row) {
         if (!err) {
             res.json(row);
         } else {
@@ -44,14 +47,23 @@ api.get("/", (req, res) => {
 });
 
 //add new entry
-api.post("/", bodyParser, (req, res) => {
-    res.send("Got a POST request");
+api.post("/", jsonParser, (req, res) => {
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const number = req.body.number;
+    db.run(
+        "INSERT INTO phone_book (first_name, last_name, number) VALUES (?, ?, ?)",
+        firstName,
+        lastName,
+        number
+    );
+    res.end();
 });
 
-// display one phone number
-api.get("/:id", (req, res) => {
-    const id = req.params["id"];
-    db.get("SELECT number FROM phone_book WHERE id=(?)", id, function(
+// listing of all the records that match search criteria for “Last Name”
+api.get("/:lastname", (req, res) => {
+    const lastName = req.params["lastname"];
+    db.get("SELECT number FROM phone_book WHERE last_name=(?)", lastName, function (
         err,
         row
     ) {
