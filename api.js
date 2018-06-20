1b94b0ab066e2a861f595e6fc27fe1e4b0d80b60
nikola <nikola.margit@gmail.com>
2018-06-20 23:05:28 +0200

    add search by last name and 404 route

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 var sqlite3 = require("sqlite3").verbose();
 var bodyParser = require("body-parser");
 var db = new sqlite3.Database(":memory:"); // database will be stored in RAM only
-db.serialize(function () {
+db.serialize(function() {
     db.run(
         "CREATE TABLE IF NOT EXISTS phone_book (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, number TEXT)"
     );
@@ -24,20 +24,25 @@ db.serialize(function () {
         "Brown",
         "+381628000111"
     );
+    db.run(
+        "INSERT INTO phone_book (first_name, last_name, number) VALUES (?, ?, ?)",
+        "Lee",
+        "Brown",
+        "+381628000111"
+    );
 });
 
 // define api object
 const express = require("express");
 const api = express();
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 //var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
 
 // define API endpoints
 
 // list all entries
 api.get("/", (req, res) => {
-    db.all("SELECT * FROM phone_book", function (err, row) {
+    db.all("SELECT * FROM phone_book", function(err, row) {
         if (!err) {
             res.json(row);
         } else {
@@ -51,24 +56,40 @@ api.post("/", jsonParser, (req, res) => {
     const firstName = req.body.first_name;
     const lastName = req.body.last_name;
     const number = req.body.number;
-    db.run(
-        "INSERT INTO phone_book (first_name, last_name, number) VALUES (?, ?, ?)",
-        firstName,
-        lastName,
-        number
-    );
-    res.end();
+    if (firstName && lastName && number) {
+        db.run(
+            "INSERT INTO phone_book (first_name, last_name, number) VALUES (?, ?, ?)",
+            firstName,
+            lastName,
+            number
+        );
+    }
+    res.send("added");
 });
 
 // listing of all the records that match search criteria for “Last Name”
-api.get("/:lastname", (req, res) => {
+api.get("/search/:lastname", (req, res) => {
     const lastName = req.params["lastname"];
-    db.get("SELECT number FROM phone_book WHERE last_name=(?)", lastName, function (
-        err,
-        row
-    ) {
-        res.json({ number: row.number });
+
+    const sql = "SELECT * FROM phone_book WHERE last_name = ? COLLATE NOCASE";
+
+    db.all(sql, [lastName], function(err, row) {
+        res.send(row);
     });
+});
+
+// delete all entries from the DB
+
+api.get("/delete", (req, res) => {
+    db.run("DELETE FROM phone_book", function(err) {
+        res.send("deleted");
+    });
+});
+
+// 404
+
+api.get("*", function(req, res) {
+    res.send("what???", 404);
 });
 
 // run server
